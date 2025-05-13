b859ee0e278a22c40ba66a419b98d8b6e3f983b4
João Pedro Cardoso <joaopev2@hotmail.com>
2025-05-13 19:30:21 -0300

    Métodos POST e GET all providos na rota de usinas

---
+++ src/app/api/usinas/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createUsina, getUsinas } from "../../../../services/usinaServices";
+import { usinaSchema } from "../../../../dtos/usinaValidationDTO";
+import { tratarErro } from "../../../../utils/tratamentoErros";
+
+
+export async function POST(request: NextRequest) {
+
+    try {
+        const body = await request.json()
+        const data = usinaSchema.parse(body);
+        const createdUsina = await createUsina(data)
+
+        return NextResponse.json({ succes: true, body: createdUsina })
+    } catch(error) {
+           return tratarErro(error);
+    }
+}
+
+export async function GET(request: NextRequest) {
+
+    try {
+        const data = await getUsinas()
+        return NextResponse.json({ succes: true, body: data })
+    } catch(error) {
+           return tratarErro(error);
+    }
+}

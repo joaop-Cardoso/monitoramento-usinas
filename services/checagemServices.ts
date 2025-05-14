45510dbc66c8ba720c1d28ec6c3fcb369fc34ff4
João Pedro Cardoso <joaopev2@hotmail.com>
2025-05-14 12:34:12 -0300

    Rota e serviço de checagem de temperatura média por inversor e dias providos

--- services/checagemServices.ts
+++ services/checagemServices.ts
@@ -76,3 +76,64 @@ export async function potenciaMaximaDia(
 
   return resultadoFormatado;
 }
+
+export async function mediaTemperaturaDia(
+  inversorId: number,
+  dataInicio: string | Date,
+  dataFim: string | Date
+) {
+  // Função para criar data em UTC
+  function criarDataUTC(dateStr: string | Date, fimDoDia = false) {
+    const data = typeof dateStr === 'string' ? dateStr : dateStr.toISOString().split('T')[0];
+    const [ano, mes, dia] = data.split('-').map(Number);
+
+    // Definindo a hora para 00:00 ou 23:59 dependendo de 'fimDoDia'
+    const horas = fimDoDia ? 23 : 0;
+    const minutos = fimDoDia ? 59 : 0;
+    const segundos = fimDoDia ? 59 : 0;
+    const milissegundos = fimDoDia ? 999 : 0;
+
+    return new Date(Date.UTC(ano, mes - 1, dia, horas, minutos, segundos, milissegundos));
+  }
+
+  const inicio = criarDataUTC(dataInicio);
+  const fim = criarDataUTC(dataFim, true);
+
+  // Definir tipo para o resultado da consulta SQL
+  type Resultado = {
+    data: string;
+    mediaTemperatura: number;
+  };
+
+  // Query SQL com tipo esperado para o resultado
+  const resultado = await prisma.$queryRawUnsafe<Resultado[]>(
+    `
+    SELECT 
+      DATE("horario") AS "data",
+      AVG("temperaturaCelsius") AS "mediaTemperaturaCelsius"
+    FROM "Checagem"
+    WHERE "inversorId" = $1
+      AND "horario" BETWEEN $2 AND $3
+    GROUP BY DATE("horario")
+    ORDER BY DATE("horario")
+    `,
+    inversorId,
+    inicio,
+    fim
+  );
+
+  // Formatando as datas para o formato "dd|MM|yyyy"
+  const resultadoFormatado = resultado.map((item) => {
+    const dataFormatada = new Date(item.data);
+    const dia = String(dataFormatada.getUTCDate()).padStart(2, '0');
+    const mes = String(dataFormatada.getUTCMonth() + 1).padStart(2, '0');
+    const ano = dataFormatada.getUTCFullYear();
+    item.data = `${dia}|${mes}|${ano}`;
+    return item;
+  });
+
+  return {
+    success: true,
+    body: resultadoFormatado,
+  };
+}

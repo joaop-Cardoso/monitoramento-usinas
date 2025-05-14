a30c5d0c737453eb39ce3af4208c33e724900db7
João Pedro Cardoso <joaopev2@hotmail.com>
2025-05-13 21:11:30 -0300

    Serviços de atualização, deleção e recuperação de registro único em banco de dados providos

--- services/usinaServices.ts
+++ services/usinaServices.ts
@@ -1,8 +1,28 @@
+import { NextRequest } from "next/server";
 import { UsinaDTO } from "../dtos/usinaValidationDTO";
 import prisma from "../lib/prisma";
 
+export  function getIdFromParams(request: NextRequest): number {
+  const url = new URL(request.url);
+  const id = url.pathname.split("/").pop(); // pega o [id] do final da URL
+  if (!id || isNaN(Number(id))) {
+    throw { status: 400, message: "ID inválido." };
+  }
+  return Number(id);
+}
+
 export async function getUsinas() {
   return prisma.usina.findMany({ include: { inversores: true } });
+}
+
+export async function getUsinaById(id: number) {
+  const exists = await prisma.usina.findUnique({ where: { id } });
+  if(!exists){
+        const error = new Error(`Usina de id ${id} não encontrada.`);
+    (error as any).status = 404;
+    throw error;
+  }
+  return exists;
 }
 
 export async function createUsina(data: UsinaDTO) {
@@ -16,4 +36,28 @@ export async function createUsina(data: UsinaDTO) {
     throw error;
   }
   return prisma.usina.create({ data });
+}
+
+export async function updateUsina(id: number, data: UsinaDTO) {
+  
+const exists = await prisma.usina.findUnique({ where: { id } });
+  if(!exists){
+        const error = new Error(`Usina de id ${id} não encontrada.`);
+    (error as any).status = 404;
+    throw error;
+  }
+    return await prisma.usina.update({
+    where: { id },
+    data,
+  });
+}
+
+export async function deleteUsina(id: number) {
+  const exists = await prisma.usina.findUnique({ where: { id } });
+  if(!exists){
+        const error = new Error(`Usina de id ${id} não encontrada.`);
+    (error as any).status = 404;
+    throw error;
+  }
+  return prisma.usina.delete({ where: { id } });
 }

1969adfd6ea68811285f5ed381a44a05be323431
João Pedro Cardoso <joaopev2@hotmail.com>
2025-05-13 21:09:22 -0300

    Estado de sucesso POST ajustado de 200 para 201

--- src/app/api/usinas/route.ts
+++ src/app/api/usinas/route.ts
@@ -3,7 +3,6 @@ import { createUsina, getUsinas } from "../../../../services/usinaServices";
 import { usinaSchema } from "../../../../dtos/usinaValidationDTO";
 import { tratarErro } from "../../../../utils/tratamentoErros";
 
-
 export async function POST(request: NextRequest) {
 
     try {
@@ -11,7 +10,7 @@ export async function POST(request: NextRequest) {
         const data = usinaSchema.parse(body);
         const createdUsina = await createUsina(data)
 
-        return NextResponse.json({ succes: true, body: createdUsina })
+        return NextResponse.json({ succes: true, body: createdUsina}, { status: 201 })
     } catch(error) {
            return tratarErro(error);
     }
@@ -21,7 +20,7 @@ export async function GET(request: NextRequest) {
 
     try {
         const data = await getUsinas()
-        return NextResponse.json({ succes: true, body: data })
+        return NextResponse.json({ succes: true, body: data})
     } catch(error) {
            return tratarErro(error);
     }
